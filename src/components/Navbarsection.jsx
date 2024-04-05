2e3c850c3293649366ee4c64505ad98dbb1b9f62
ssomeshg <121214623+ssomeshg@users.noreply.github.com>
2024-04-05 16:24:48 +0530

    nav issue fixed

--- src/components/Navbarsection.jsx
+++ src/components/Navbarsection.jsx
@@ -8,7 +8,7 @@ function Navbar() {
             <div className="backdrop-blur-md sticky top-0 z-10">
                 <div className="px-3 md:px-0 py-5 flex justify-between items-center container mx-auto ">
                     <h1 className="uppercase font-bold text-lg text-purple-950">PORTFOLIO<span style={{color:"#3AA700"}}>.</span></h1>
-                    <div className="hidden md:block flex gap-7 justify-end items-center">
+                    <div className=" flex gap-7 justify-end items-center hidden md:flex">
                         <Link to={"/"} className="text-[16px] font-semibold hover:text-pink-950">Home</Link>
                         <Link to={"/about"} className="text-[16px] font-semibold">About me</Link>
                        
